262a8f874434227cfc5468d613f3d9e070332969
vmin <v.mingazov@gmail.com>
2023-11-05 15:29:35 +0000

    Update settings.py

--- books/settings.py
+++ books/settings.py
@@ -7,3 +7,17 @@ NEWSPIDER_MODULE = 'books.spiders'
 
 ROBOTSTXT_OBEY = True
 HTTPCACHE_ENABLED = True
+
+DOWNLOADER_MIDDLEWARES = {
+        'scrapy_splash.SplashCookiesMiddleware': 723,
+        'scrapy_splash.SplashMiddleware': 725,
+        'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
+    }
+
+SPIDER_MIDDLEWARES = {
+        'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
+    }
+
+DUPEFILTER_CLASS = 'scrapy_splash.SplashAwareDupeFilter'
+
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
